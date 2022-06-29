cc5e732d62c584092a9475fe59f0418fb7010a67
Paul Winkeler <107127234+winkeler@users.noreply.github.com>
2022-06-29 13:38:29 -0500

    Update plot1.R
    Final script creating plot 1

--- plot1.R
+++ plot1.R
@@ -1,3 +1,10 @@
+png(filename = "plot1.png")
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
 data <- mutate(data, Date = as.Date(Date, format=c("%d/%m/%Y")))
 data <- filter(data, Date == "2007-02-01" | Date == "2007-02-02")
+
+hist(as.numeric(data$Global_active_power), 
+     col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+dev.off()
